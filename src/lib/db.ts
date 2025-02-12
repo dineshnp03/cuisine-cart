f40b6c90eae3416ee407608c60412bc726365511
dineshnp03 <dineshnpcan@gmail.com>
2025-02-11 21:24:37 -0500

    db changes

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,18 +1,26 @@
 import mongoose from "mongoose";
 
-const MONGODB_URI = process.env.MONGODB_URI!;
+// connection string is from the .env variable
+const MONGODB_URI = process.env.MONGODB_URI || "";
 
-if (!MONGODB_URI) throw new Error("MONGODB_URI is missing in .env.local");
+// Checking if connection string is provided in the .env file
+if (!MONGODB_URI) {
+  throw new Error("MONGODB_URI is missing in .env.local");
+}
 
-global.mongooseCache = global.mongooseCache || { conn: null, promise: null };
-
+// MongoDB connection function
 export const connectToDatabase = async () => {
-  if (global.mongooseCache.conn) return global.mongooseCache.conn;
+  if (mongoose.connections[0].readyState) {
+    console.log("Already connected to MongoDB");
+    return mongoose.connections[0];  // to use the existing connection if exists
+  }
 
-  global.mongooseCache.promise = mongoose.connect(MONGODB_URI, {
-    dbName: "myDatabase",
-  });
-
-  global.mongooseCache.conn = await global.mongooseCache.promise;
-  return global.mongooseCache.conn;
+  try {
+    await mongoose.connect(MONGODB_URI);
+    console.log("Successfully connected to MongoDB");
+    return mongoose.connections[0];
+  } catch (error) {
+    console.error("MongoDB connection error:", error);
+    process.exit(1);  
+  }
 };
